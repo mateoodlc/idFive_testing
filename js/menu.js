6f78aa45d129101775e92a542e39e5242d4d46d8
Mateo <mateoodlc@gmail.com>
2018-12-22 18:06:28 -0500

    feat: menu fixed

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,13 @@
 let menuOpen = document.getElementById("menu-button");
+let closeButton = document.getElementById("close-button");
 menuOpen.addEventListener('click', openMenu);
+closeButton.addEventListener('click', closeMenu);
+let menuContainer = document.getElementById("menu-container");
 
 function openMenu() {
-    var menuContainer = document.getElementById("menu-container");
     menuContainer.classList.add("menu-opened")
+}
+
+function closeMenu() {
+    menuContainer.classList.remove("menu-opened");    
 }
