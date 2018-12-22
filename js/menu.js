8507bbcd7088716fd10aab078f214b6247c075bd
Mateo <mateoodlc@gmail.com>
2018-12-22 17:53:39 -0500

    feat: footer solved

---
+++ js/menu.js
@@ -0,0 +1,6 @@
+let menuOpen = document.getElementById("menu-button");
+
+function openMenu() {
+    var menuContainer = document.getElementById("menu-container");
+    menuContainer.classList.add("menu-opened")
+}
